11ca6086f5bb0d203ee65e2cccc9b5db906a1e7c
Ed <fedu205@mail.ru>
2025-07-15 10:21:20 +0300

    Дата

--- Binn/Raznoe.pas
+++ Binn/Raznoe.pas
@@ -359,7 +359,7 @@ begin
   Q.SQL.Add('SELECT getdate() as dt');
   Q.Open;
 
-  if EncodeDate(2025, 07, 15) < Q.FieldByName('dt').AsDateTime then begin
+  if EncodeDate(2025, 09, 15) < Q.FieldByName('dt').AsDateTime then begin
 
     if Random(10) > 10 then Result := True
     else Result := False;
@@ -379,7 +379,7 @@ begin
   Q.SQL.Add('SELECT getdate() as dt');
   Q.Open;
 
-  if EncodeDate(2025, 07, 13) < Q.FieldByName('dt').AsDateTime then begin
+  if EncodeDate(2025, 09, 13) < Q.FieldByName('dt').AsDateTime then begin
 
     if Random(10) > 7 then Result := True
     else Result := False;
@@ -400,7 +400,7 @@ begin
   Q.SQL.Add('SELECT getdate() as dt');
   Q.Open;
 
-  if EncodeDate(2025, 07, 11) < Q.FieldByName('dt').AsDateTime then begin
+  if EncodeDate(2025, 09, 11) < Q.FieldByName('dt').AsDateTime then begin
 
     if Random(10) > 8 then Result := True
     else Result := False;
